8f50b7ece20941de4a8359653aed0b8ddbe05c0d
Caio Willian <cwillian40@hotmail.com>
2019-05-23 22:36:58 -0300

    Instalação e configuração do Hooks

--- src/main.js
+++ src/main.js
@@ -1,2 +1,8 @@
-const a = "caio";
+var should = require( 'should' );
+var sum = require( '../sum' );
 
+describe( 'sum.js', function() {
+  it( 'sum of 2 + 3 should return 5', function() {
+    sum( 2, 3 ).should.be.equal( 5 );
+  });
+});
